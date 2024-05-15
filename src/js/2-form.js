dc5efd29a5e2dccb3f05f7e5a02db7341c549921
kkkvicki <kindrakevych@ukr.net>
2024-05-15 16:28:30 +0200

    hm9

---
+++ src/js/2-form.js
@@ -0,0 +1,35 @@
+const formData = {
+  email: '',
+  message: '',
+};
+
+const feedbackForm = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('.feedback-form > label > input');
+const messageInput = document.querySelector(
+  '.feedback-form > label > textarea'
+);
+
+const storedData = localStorage.getItem('feedback-form-state');
+if (storedData) {
+  const parsedData = JSON.parse(storedData);
+  emailInput.value = parsedData.email;
+  messageInput.value = parsedData.message;
+}
+
+feedbackForm.addEventListener('input', event => {
+  const { name, value } = event.target;
+  formData[name] = value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+feedbackForm.addEventListener('submit', event => {
+  event.preventDefault();
+  if (formData.email.trim() === '' || formData.message.trim() === '') {
+    alert('Fill please all fields');
+  } else {
+    console.log(formData);
+    localStorage.removeItem('feedback-form-state');
+    emailInput.value = '';
+    messageInput.value = '';
+  }
+});
